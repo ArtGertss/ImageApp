c30c2a9621cb11348aa0a4c1f744cfed796a6d97
ArtGertss <gerts.artem2@gmail.com>
2022-07-04 21:18:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/imageapp/ui/gallery/DetailsFragment.kt
@@ -0,0 +1,97 @@
+package com.example.imageapp.ui.gallery
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.example.imageapp.R
+import com.example.imageapp.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DetailsFragment()
+    }
+
+
+    private lateinit var viewModel: DetailsViewModel
+
+    private val args by navArgs<DetailsFragmentArgs>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            val photo = args.photo
+
+            Glide.with(this@DetailsFragment)
+                .load(photo.urls.full)
+                .error(R.drawable.ic_baseline_error_24)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        textViewCreator.isVisible = true
+                        textViewDescription.isVisible = photo.description != null
+                        return false
+                    }
+                })
+                .into(imageView)
+
+            textViewDescription.text = photo.description
+
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            textViewCreator.apply {
+                text = "Photo by ${photo.user.name} on Unsplash"
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/imageapp/ui/gallery/DetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.imageapp.ui.gallery
+
+import androidx.lifecycle.ViewModel
+
+
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
